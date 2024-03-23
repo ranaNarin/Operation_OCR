f12088e7e167bbd1a78ecaa9ce9355e76226f4c6
Narinder Rana <rana.narin@gmail.com>
2024-03-23 11:56:23 +0530

    Initial commit

---
+++ src/main/java/org/alphamoney/operation_ocr/OperationOcrApplication.java
@@ -0,0 +1,13 @@
+package org.alphamoney.operation_ocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationOcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperationOcrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alphamoney/operation_ocr/OperationOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alphamoney.operation_ocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationOcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
